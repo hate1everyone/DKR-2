f60ab4e7e3201c0d933cf2dd175ebbf014228641
hate1everyone <118364577+hate1everyone@users.noreply.github.com>
2022-12-16 01:30:36 +0300

    Add files via upload

---
+++ дк 2 (3).pas
@@ -0,0 +1,16 @@
+﻿var
+  x: array [1..20] of integer;
+  s: string;
+  i: integer;
+begin
+  s := '';
+  for i := 1 to 19 do
+    begin
+      x[i] := random(10);
+      write(x[i], ',');
+      if Pos(char(x[i]), s) < 1 then
+        s := s + char(x[i]);
+    end;
+  writeLn(x[i]);
+  writeLn('Различных - ', length(s));
+end.
